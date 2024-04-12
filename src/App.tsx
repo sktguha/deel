41525e28a01082539cc56495d2096b100bdf8377
Saikat Guha <sktguha@gmail.com>
2024-04-13 00:19:06 +0530

    wip add different use cases

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,24 @@ import { Driver } from "./components/Driver";
 
 
 export default function App(){
-  return <Driver />
+  return <div className="main-container">
+    <section>
+      <h2>Base example</h2>
+      <Driver />
+    </section>
+    <section>
+      <h2>Base example multi selection allowed</h2>
+      <Driver />
+    </section>
+    <section>
+      <h2>Single Selection only</h2>
+      <Driver single/>
+    </section>
+    <section>
+      <h2>Required field example ( critical in form validations , form library integrations etc)</h2>
+      <Driver required/>
+    </section>
+  </div>
+  
 }
 
--- src/components/Driver.tsx
+++ src/components/Driver.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { AutoComplete } from "./AutoComplete";
 
-export function Driver() {
+export function Driver({single ,required, errorTextProp}) {
   const defaultOptions = [{
     label: "Hello",
     id: 1
@@ -16,11 +16,16 @@ export function Driver() {
   }
   ];
   const [selectedOptions, setSelectedOptions] = useState([]);
-  return <AutoComplete selectedOptions={selectedOptions}
+  const [errorText, setErrorText] = useState(required ? "This is a required field" : false);
+  return <AutoComplete errorText={errorTextProp || errorText} single={single} selectedOptions={selectedOptions}
     getOptions={(text) => new Promise((res) => setTimeout(() => res(defaultOptions), Math.random() * 2500))}
     onSelectedOptionsChange={(options) => {
       console.log("new options is ", options);
-      // if options.length === 0 && required is true, then setErrorText("This is a required field")
+      if (required && options.length === 0) { 
+        setErrorText("This is a required field");
+      } else {
+        setErrorText(false);
+      }
       setSelectedOptions(options);
     }} />;
 }
