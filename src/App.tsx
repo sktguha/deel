b0641cbcea97b97fccc77707c974384d0e8c6eaf
Saikat Guha <sktguha@gmail.com>
2024-04-12 23:32:42 +0530

    add basic autocomplete version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-nocheck
+import "./App.css";
+import { useState  } from "react";
 
-function App() {
+
+function AutoComplete({placeholder="autocomplete", 
+  getOptions, 
+  predetermindedOptions, 
+  selectedOptions=[], 
+  onSelectedOptionsChange}) {
+  const [availableOptions, setAvailableOptions ] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <input type="text" placeholder={placeholder} onChange={
+        (e)=>{
+          const text = e.target.value;
+          console.log(text);
+          // setLoading(true)
+          getOptions(text).then((options)=>{
+              // setLoading(false)
+              console.log(options);
+              const availOptions = options.filter(option=>option.label.indexOf(text)!==-1);
+              setAvailableOptions(availOptions);
+            }, (err)=>{ 
+              // here can integrate with a logging library
+              console.error("error occured: ", err);
+              });
+          // setAvailableOptions([{label: text}])
+          
+        }
+      }/>
+      {JSON.stringify(selectedOptions)}
+      {
+        <div>{
+          availableOptions.map(option=><div onClick={()=>{
+            alert(option.label);
+            onSelectedOptionsChange([...selectedOptions,option])
+          }} key={option.id}>{option.label}</div>)
+          }
+          </div>
+      }
     </div>
   );
 }
 
-export default App;
+export default function App(){
+  const defaultOptions = [{
+    label: "Hello",
+    id:1 
+  },
+  {
+    label: "Help",
+    id:2 
+  },
+  {
+    label: "Helldivers",
+    id:3 
+  }
+]
+const [selectedOptions, setOptions ] = useState([])
+  return <AutoComplete selectedOptions={selectedOptions} 
+  getOptions={
+    (text)=>new Promise((res)=>setTimeout(()=>res(defaultOptions),Math.random()*2500))
+  }
+  onSelectedOptionsChange={(options)=>{
+    console.log("new options is ",options);
+    setOptions(options);
+  }}/>
+}
+
