875b10e31f3dec4ff9ba9bbd72ceef4069d4e85d
Saikat Guha <sktguha@gmail.com>
2024-04-13 21:46:31 +0530

    add tests

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Driver from './App';
-
-test('renders learn react link', () => {
-  render(<Driver />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
