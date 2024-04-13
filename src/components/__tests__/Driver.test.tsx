a1f6e42f0d9eaf4c40ce13460cda649fb3140b95
Saikat Guha <sktguha@gmail.com>
2024-04-13 22:05:27 +0530

    add tests

--- src/components/__tests__/Driver.test.tsx
+++ src/components/__tests__/Driver.test.tsx
@@ -7,3 +7,18 @@ test("Driver base",()=>{
     const { container } = render(<Driver />)
     expect(container).toMatchSnapshot();
 })
+
+test("Driver single prop",()=>{
+  const { container } = render(<Driver single/>)
+  expect(container).toMatchSnapshot();
+})
+
+test("Driver required",()=>{
+  const { container } = render(<Driver required/>)
+  expect(container).toMatchSnapshot();
+})
+
+test("Driver errorText",()=>{
+  const { container } = render(<Driver errorTextProp={"custom errorText for test"}/>)
+  expect(container).toMatchSnapshot();
+})
