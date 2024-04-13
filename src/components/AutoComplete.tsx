3cc2aa9c07688f3de76e467868bd0c5b3a3aa5a1
Saikat Guha <sktguha@gmail.com>
2024-04-13 21:17:14 +0530

    add error boundary

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -12,7 +12,7 @@ export function AutoComplete({
   getOptions,
   maxDisplayItems = 12, selectedOptions = [], onSelectedOptionsChange }) {
   const [availableOptions, setAvailableOptions] = useState([]);
-  const [loading, setLoading ] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const inputRef = useRef();
   const itemsRef = useRef();
@@ -28,53 +28,55 @@ export function AutoComplete({
   }
 
   return (
-    <div className="App">
-      <div>{label}</div>
-      {errorText && <div className="error-text">{errorText}</div>}
-      <div className="input-container">
-      <input type="text"
-        className={"input-box " + (errorText && "input-box-error")}
-        disabled={single && selectedOptions.length >= 1}
-        onBlur={(e) => {
-          console.log(e);
-          console.log("body blur");
-          setTimeout(() => {
-            itemsRef.current.style.display = "none"
-          }, 300);
-        }} tabIndex={-1}
-        ref={inputRef} placeholder={placeholder} onClick={() => showItems(true)} onChange={debounce((e) => {
-          setLoading(true);
-          getOptions(e.target.value).then((options) => {
-            setLoading(false)
-            const text = e.target.value;
-            console.log(text);
-            console.log(options);
-            const availOptions = options.filter(option => option.label.toLowerCase().indexOf(text.toLowerCase()) !== -1);
-            console.log(availOptions);
-            setAvailableOptions(availOptions);
-            showItems();
-          }, (err) => {
-            // here can integrate with a logging library
-            console.error("error occured: ", err);
-          });
-        })} />
-        {loading && <img width="33" height="33" src={loader} />}
+    <ErrorBoundary>
+      <div className="Autocomplete">
+        <div>{label}</div>
+        {errorText && <div className="error-text">{errorText}</div>}
+        <div className="input-container">
+          <input type="text"
+            className={"input-box " + (errorText && "input-box-error")}
+            disabled={single && selectedOptions.length >= 1}
+            onBlur={(e) => {
+              console.log(e);
+              console.log("body blur");
+              setTimeout(() => {
+                itemsRef.current.style.display = "none"
+              }, 300);
+            }} tabIndex={-1}
+            ref={inputRef} placeholder={placeholder} onClick={() => showItems(true)} onChange={debounce((e) => {
+              setLoading(true);
+              getOptions(e.target.value).then((options) => {
+                setLoading(false)
+                const text = e.target.value;
+                console.log(text);
+                console.log(options);
+                const availOptions = options.filter(option => option.label.toLowerCase().indexOf(text.toLowerCase()) !== -1);
+                console.log(availOptions);
+                setAvailableOptions(availOptions);
+                showItems();
+              }, (err) => {
+                // here can integrate with a logging library
+                console.error("error occured: ", err);
+              });
+            })} />
+          {loading && <img width="33" height="33" src={loader} />}
+        </div>
+        {/* {JSON.stringify(selectedOptions)} */}
+        <div className="selected-options">
+          {selectedOptions.map((option) => {
+            return (<div key={option.id} className="pill-item">
+              <span>{option.label}</span>
+              <span onClick={() => removeOption(option.id)} className="pill-item-cross"><b> x</b></span>
+            </div>);
+          })}
+        </div>
+        <div onClick={() => {
+          // alert("onclick");
+        }}>
+          <ItemsDisplay {...{ itemsRef, availableOptions, maxDisplayItems, selectedOptions, onSelectedOptionsChange, getBoldedText, inputRef }} />
+        </div>
+        {/* {ItemsDisplay(itemsRef, availableOptions, maxDisplayItems, selectedOptions, onSelectedOptionsChange, getBoldedText, inputRef)} */}
       </div>
-      {/* {JSON.stringify(selectedOptions)} */}
-      <div className="selected-options">
-        {selectedOptions.map((option) => {
-          return (<div key={option.id} className="pill-item">
-            <span>{option.label}</span>
-            <span onClick={() => removeOption(option.id)} className="pill-item-cross"><b> x</b></span>
-          </div>);
-        })}
-      </div>
-      <div onClick={() => {
-        // alert("onclick");
-      }}>
-        <ItemsDisplay {...{ itemsRef, availableOptions, maxDisplayItems, selectedOptions, onSelectedOptionsChange, getBoldedText, inputRef }} />
-      </div>
-      {/* {ItemsDisplay(itemsRef, availableOptions, maxDisplayItems, selectedOptions, onSelectedOptionsChange, getBoldedText, inputRef)} */}
-    </div>
+    </ErrorBoundary>
   );
 }
---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+  
+    static getDerivedStateFromError(error) {
+      return { hasError: true };
+    }
+  
+    render() {
+      if (this.state.hasError) {
+        return <h2>Something went wrong in Autocomplete component. please try again</h2>;
+      }
+  
+      return this.props.children; 
+    }
+  }
