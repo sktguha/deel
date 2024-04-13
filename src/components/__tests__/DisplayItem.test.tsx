c7c41805bac42ca7aec324aa69616523e8305846
Saikat Guha <sktguha@gmail.com>
2024-04-13 21:39:56 +0530

    add basic test snapshot

--- src/components/__tests__/DisplayItem.test.tsx
+++ src/components/__tests__/DisplayItem.test.tsx
@@ -4,6 +4,6 @@ import { render , screen } from '@testing-library/react';
 
 
 test("DisplayItem base",()=>{
-    const { container } = render(<DisplayItem />)
+    const { container } = render(<DisplayItem option={{label: "test", id: 1}}/>)
     expect(container).toMatchSnapshot();
 })
