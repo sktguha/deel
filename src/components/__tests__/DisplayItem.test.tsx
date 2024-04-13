381e141d828521b132bdeb699df9231495b21e87
Saikat Guha <sktguha@gmail.com>
2024-04-13 21:45:25 +0530

    add tests

--- src/components/__tests__/DisplayItem.test.tsx
+++ src/components/__tests__/DisplayItem.test.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { DisplayItem } from "../DisplayItem"
 import { render , screen } from '@testing-library/react';
+import getBoldedText from '../../utils/getBoldedText';
 
 
 test("DisplayItem base",()=>{
-    const { container } = render(<DisplayItem option={{label: "test", id: 1}}/>)
+    const { container } = render(<DisplayItem getBoldedText={getBoldedText} inputRef={{ current: { value: "te"}}} option={{label: "test", id: 1}}/>)
     expect(container).toMatchSnapshot();
 })
 
 test("DisplayItem last item",()=>{
-  const { container } = render(<DisplayItem isLast option={{label: "test", id: 1}}/>)
+  const { container } = render(<DisplayItem getBoldedText={getBoldedText} isLast inputRef={{ current: { value: "te"}}} option={{label: "test", id: 1}}/>)
   expect(container).toMatchSnapshot();
 })
