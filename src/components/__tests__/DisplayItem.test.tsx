4d2a3b214691daad18a2637588d1ad8f3ab2b33f
Saikat Guha <sktguha@gmail.com>
2024-04-13 22:01:48 +0530

    add tests

--- src/components/__tests__/DisplayItem.test.tsx
+++ src/components/__tests__/DisplayItem.test.tsx
@@ -13,3 +13,15 @@ test("DisplayItem last item",()=>{
   const { container } = render(<DisplayItem getBoldedText={getBoldedText} isLast inputRef={{ current: { value: "te"}}} option={{label: "test", id: 1}}/>)
   expect(container).toMatchSnapshot();
 })
+
+test("DisplayItem multiple options",()=>{
+  const { container } = render(<DisplayItem getBoldedText={getBoldedText} isLast inputRef={{ current: { value: "te"}}} 
+  option={{label: "test2", id: 2}}/>)
+  expect(container).toMatchSnapshot();
+})
+
+test("DisplayItem multiple options last item",()=>{
+  const { container } = render(<DisplayItem isLast getBoldedText={getBoldedText} isLast inputRef={{ current: { value: "te"}}} 
+  option={{label: "test2", id: 2}}/>)
+  expect(container).toMatchSnapshot();
+})
--- src/components/__tests__/ItemsDisplay.test.tsx
+++ src/components/__tests__/ItemsDisplay.test.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { ItemsDisplay } from "../ItemsDisplay"
-import { render , screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import getBoldedText from '../../utils/getBoldedText';
 
 
-test("ItemsDisplay base",()=>{
-    const { container } = render(<ItemsDisplay getBoldedText={getBoldedText} inputRef={{ current: { value: "te"}}} availableOptions={[{id:1, label: "test"}]}/>)
-    expect(container).toMatchSnapshot();
+test("ItemsDisplay base", () => {
+  const { container } = render(<ItemsDisplay getBoldedText={getBoldedText} inputRef={{ current: { value: "te" } }} availableOptions={[{ id: 1, label: "test" }]} />)
+  expect(container).toMatchSnapshot();
+})
+
+test("ItemsDisplay multiple options", () => {
+  const { container } = render(<ItemsDisplay getBoldedText={getBoldedText} inputRef={{ current: { value: "te" } }}
+    availableOptions={[
+      { id: 1, label: "test" },
+      { id: 2, label: "test2" }]
+    } />)
+  expect(container).toMatchSnapshot();
 })
