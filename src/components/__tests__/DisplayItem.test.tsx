e0746b9e417b3c288305945086407b74c2a55230
Saikat Guha <sktguha@gmail.com>
2024-04-13 21:40:26 +0530

    add basic test snapshot

--- src/components/__tests__/DisplayItem.test.tsx
+++ src/components/__tests__/DisplayItem.test.tsx
@@ -7,3 +7,8 @@ test("DisplayItem base",()=>{
     const { container } = render(<DisplayItem option={{label: "test", id: 1}}/>)
     expect(container).toMatchSnapshot();
 })
+
+test("DisplayItem last item",()=>{
+  const { container } = render(<DisplayItem isLast option={{label: "test", id: 1}}/>)
+  expect(container).toMatchSnapshot();
+})
