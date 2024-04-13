07c410be8c96d6ad1a3931cc6df35399f2d54279
Saikat Guha <sktguha@gmail.com>
2024-04-13 22:07:53 +0530

    add tests

--- src/components/__tests__/AutoComplete.test.tsx
+++ src/components/__tests__/AutoComplete.test.tsx
@@ -7,3 +7,18 @@ test("Autocomplete base",()=>{
     const { container } = render(<AutoComplete />)
     expect(container).toMatchSnapshot();
 })
+
+test("Autocomplete single prop",()=>{
+    const { container } = render(<AutoComplete single/>)
+    expect(container).toMatchSnapshot();
+  })
+  
+  test("Autocomplete required",()=>{
+    const { container } = render(<AutoComplete errorText={"this is a required field"}/>)
+    expect(container).toMatchSnapshot();
+  })
+  
+  test("Autocomplete errorText",()=>{
+    const { container } = render(<AutoComplete errorText={"custom errorText for test"}/>)
+    expect(container).toMatchSnapshot();
+  })
