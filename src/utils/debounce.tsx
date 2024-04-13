910f311517b4f7c4d8cad43c28291bd56040a3a4
Saikat Guha <sktguha@gmail.com>
2024-04-13 22:46:39 +0530

    ts errors fixed

--- src/utils/debounce.tsx
+++ src/utils/debounce.tsx
@@ -1,7 +1,10 @@
-export default function debounce(func, timeout = 300) {
-    let timer;
-    return (...args) => {
+import { genericFn } from "../components/AutoComplete.types";
+
+export default function debounce(func: genericFn, timeout = 300) {
+    let timer: number;
+    return (...args: any[]) => {
       clearTimeout(timer);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+      // @ts-expect-error
+      timer = setTimeout(() => { func.apply(this, args); }, timeout) as unknown as number;
     };
 }
--- src/utils/getBoldedText.tsx
+++ src/utils/getBoldedText.tsx
@@ -1,4 +1,4 @@
-export default function getBoldedText(label, text) {
+export default function getBoldedText(label: string, text: string) {
     return <div dangerouslySetInnerHTML={{
       __html: label.split(text).join("<b>" + text + "</b>")
     }} />;
