ad46d632212e4bc537a03f772f4629f716ff99a7
Saikat Guha <sktguha@gmail.com>
2024-04-13 22:09:30 +0530

    add tests

--- src/components/__tests__/ErrorBoundary.test.tsx
+++ src/components/__tests__/ErrorBoundary.test.tsx
@@ -7,3 +7,12 @@ test("ErrorBoundary base",()=>{
     const { container } = render(<ErrorBoundary />)
     expect(container).toMatchSnapshot();
 })
+
+test("ErrorBoundary error component",()=>{
+  const Component = ()=>{
+    throw new Error("test error");
+    return <div>Test Component</div>
+  }
+  const { container } = render(<ErrorBoundary><Component /></ErrorBoundary>)
+  expect(container).toMatchSnapshot();
+})
