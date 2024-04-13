144384bdf47e8116064a090835e38f45274892fe
Saikat Guha <sktguha@gmail.com>
2024-04-13 21:53:55 +0530

    add tests

--- src/components/__tests__/ItemsDisplay.test.tsx
+++ src/components/__tests__/ItemsDisplay.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { ItemsDisplay } from "../ItemsDisplay"
 import { render , screen } from '@testing-library/react';
+import getBoldedText from '../../utils/getBoldedText';
 
 
 test("ItemsDisplay base",()=>{
-    const { container } = render(<ItemsDisplay />)
+    const { container } = render(<ItemsDisplay getBoldedText={getBoldedText} inputRef={{ current: { value: "te"}}} availableOptions={[{id:1, label: "test"}]}/>)
     expect(container).toMatchSnapshot();
 })
